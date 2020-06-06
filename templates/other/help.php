eccf6b20e2cc24826939b823d7a47d8634cb6284
Luiz Amichi <luizamichi@gmail.com>
2020-06-06 14:55:00 -0300

    Settings of the company that uses the software

---
+++ templates/other/help.php
@@ -0,0 +1,28 @@
+	<!-- MODAL DE AJUDA -->
+	<div class="bd-example-modal-lg fade modal" id="help-modal">
+		<div class="modal-dialog modal-dialog-centered modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h3 class="modal-title">Ajuda</h3>
+					<button class="close" data-dismiss="modal" type="button">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<p>&#128221; <strong>Razão Social:</strong> obrigatório, tamanho mínimo (2), tamanho máximo (32).</p>
+					<p>&#129498; <strong>Nome Fantasia:</strong> obrigatório, tamanho mínimo (2), tamanho máximo (64).</p>
+					<p>&#128209; <strong>CNPJ:</strong> obrigatório, tamanho (18).</p>
+					<p>&#128231; <strong>E-mail:</strong> opcional, tamanho mínimo (4), tamanho máximo (32).</p>
+					<p>&#128222; <strong>Telefone:</strong> obrigatório, tamanho (14).</p>
+					<p>&#128205; <strong>CEP:</strong> obrigatório, tamanho (10).</p>
+					<p>&#128204; <strong>Bairro:</strong> obrigatório, tamanho mínimo (4), tamanho máximo (32).</p>
+					<p>&#127961; <strong>Cidade:</strong> obrigatório, tamanho mínimo (4), tamanho máximo (64).</p>
+					<p>&#127987; <strong>Estado:</strong> obrigatório.</p>
+					<p>&#127968; <strong>Endereço:</strong> obrigatório, tamanho mínimo (4), tamanho máximo (64).</p>
+					<p>&#128290; <strong>Número:</strong> obrigatório, tamanho mínimo (2), tamanho máximo (8).</p>
+					<p>&#127760; <strong>Website:</strong> opcional, tamanho mínimo (8), tamanho máximo (32).</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/ MODAL DE AJUDA -->
