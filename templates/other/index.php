2122cafb9f400109560a19d8b0cebf551852f13d
Luiz Amichi <luizamichi@gmail.com>
2022-03-01 18:35:48 -0300

    Most used templates for inclusion

---
+++ templates/other/index.php
@@ -0,0 +1,26 @@
+	<?php include_once(HEADER); ?>
+
+	<div class="container mb-5">
+		<!-- CABEÇALHO -->
+		<div class="my-3 text-center">
+			<img alt="Small Trade Manager" class="img-fluid mx-3" height="75" src="<?=BASE_URL?>img/index-color.svg" width="75"/>
+			<h2>Small Trade Manager</h2>
+			<hr/>
+		</div>
+		<!--/ CABEÇALHO -->
+
+		<?php include_once(FLASH); ?>
+
+		<!-- TEXTO DE APRESENTAÇÃO -->
+		<p class="lead text-justify">O Small Trade Manager é ideal para o gerenciamento de qualquer empresa, independente do seu segmento de mercado e porte que tenha, as lojas são empreendimentos que precisam, todos os dias, lidar com um grande fluxo de informação. E para conseguir manter tudo em ordem, funcionando de maneira adequada, não basta contar com uma equipe de funcionários competente. Ela precisa ter a ajuda de ferramentas que não só possibilitem o seu trabalho como o torne mais eficiente.</p>
+		<p class="lead text-justify">Nesse sentido, o Small Trade Manager é uma ferramenta de extrema necessidade, que colabora com o desempenho da equipe do seu negócio de uma série de maneiras. Ele funciona como um sistema de gestão empresarial que não só ajuda no setor de vendas, que é especialmente importante para as lojas, como também a administrar os demais departamentos, tornando a rotina da sua empresa muito mais dinâmica.</p>
+		<p class="lead text-justify">O Small Trade Manager é um software ERP que permite que todos os dados da sua empresa sejam informatizados, para que possam ser armazenados em bancos de dados e fáceis de serem acessados quando preciso. Também conecta os setores para que as tarefas possam ser compartilhadas, reduzindo o tempo que cada processo demora a ser concluído. Isso é possível porque não é preciso digitar de novo os dados que outros funcionários já incluíram no sistema.</p>
+		<p class="lead text-justify">Além disso, o sistema de gestão empresarial Small Trade Manager possibilita que algumas etapas do trabalho da sua equipe sejam automáticas, tornando tudo mais eficiente, ou seja, mais rápido sem que haja perda de qualidade. É possível até mesmo aumentar a qualidade com que as tarefas são feitas, pois os dados são acessados mais rapidamente, as informações salvas com segurança, clientes atendidos com agilidade e muitas outras vantagens.</p>
+		<!--/ TEXTO DE APRESENTAÇÃO -->
+	</div>
+
+	<?php include_once(FOOTER); ?>
+
+</body>
+
+</html>
