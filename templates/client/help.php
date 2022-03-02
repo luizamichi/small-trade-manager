edbf01ca355d2436c7428beda46d051748b10c96
Luiz Amichi <luizamichi@gmail.com>
2022-03-02 07:20:06 -0300

    Client MVC

---
+++ templates/client/help.php
@@ -0,0 +1,33 @@
+	<!-- MODAL DE AJUDA -->
+	<div class="bd-example-modal-lg fade modal" id="help-modal">
+		<div class="modal-dialog modal-dialog-centered modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h3 class="modal-title">Ajuda</h3>
+					<button class="close" data-dismiss="modal" type="button">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<p>&#128129; <strong>Nome:</strong> obrigatório, tamanho mínimo (2), tamanho máximo (32).</p>
+					<p>&#129335; <strong>Sobrenome:</strong> obrigatório, tamanho mínimo (2), tamanho máximo (32).</p>
+					<p>&#128195; <strong>RG:</strong> opcional, tamanho mínimo (11), tamanho máximo (12).</p>
+					<p>&#128196; <strong>CPF:</strong> obrigatório, tamanho (14).</p>
+					<p>&#128197; <strong>Data de Nascimento:</strong> obrigatório, data inferior ou igual a atual (<?=date("d/m/Y")?>).</p>
+					<p>&#128205; <strong>CEP:</strong> opcional, tamanho (9).</p>
+					<p>&#128204; <strong>Bairro:</strong> opcional, tamanho mínimo (4), tamanho máximo (32).</p>
+					<p>&#127961; <strong>Cidade:</strong> obrigatório, tamanho mínimo (4), tamanho máximo (64).</p>
+					<p>&#127987; <strong>Estado:</strong> obrigatório.</p>
+					<p>&#127968; <strong>Endereço:</strong> obrigatório, tamanho mínimo (4), tamanho máximo (64).</p>
+					<p>&#128290; <strong>Número:</strong> obrigatório, tamanho mínimo (2), tamanho máximo (8).</p>
+					<p>&#10133; <strong>Complemento:</strong> opcional, tamanho mínimo (4), tamanho máximo (32).</p>
+					<p>&#128231; <strong>E-mail:</strong> opcional, tamanho mínimo (4), tamanho máximo (32).</p>
+					<p>&#128222; <strong>Telefone:</strong> opcional, tamanho (14).</p>
+					<p>&#128242; <strong>Celular:</strong> opcional, tamanho (15).</p>
+					<p>&#128699; <strong>Sexo:</strong> obrigatório.</p>
+					<p>&#128172; <strong>Observação:</strong> opcional, tamanho mínimo (4), tamanho máximo (512).</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--/ MODAL DE AJUDA -->
